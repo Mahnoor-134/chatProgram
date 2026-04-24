6ecf17338b25078431d41cb3a3b88efb12a42f10
okama01 <okama01@HUGI0255S.adm.huddinge.se>
2026-04-24 10:31:31 +0200

    24/04

---
+++ src/Server.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author okama01
+ */
+import java.net.*;
+import java.io.*;
+
+public class Server {
+
+    
+    private ServerSocket serverSocket = null; 
+    private Socket clientSocket = null; 
+    private DataInputStream in = null;
+    private PrintWriter out; 
+    private BufferedReader inn; 
+    
+    
+    
+    
+    public Server (int port){
+        
+        
+    }
+}
--- src/chatprogram/ChatProgram.java
+++ src/chatprogram/ChatProgram.java
@@ -3,9 +3,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package chatprogram;
+
 import java.net.*;
 import java.io.*;
-
 
 /**
  *
@@ -17,10 +17,14 @@ public class ChatProgram {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        
-        
-        
+
+        //a server socket 
+        //    ServerSocket serverSocket = new ServerSocket(12345);
+        //accept incoming client commection
+        //  Socket clientSocket = serverSocket.accept();
+        //input stream to send and receive data
+        //InputStream input = clientSocket.getInputStream(); 
+        //OutputStream output = clientSocket.getOutputStream();
     }
-    
+
 }
