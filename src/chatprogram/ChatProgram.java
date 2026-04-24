9c891205a34c978ca881a805d0a92a9084cc3660
okama01 <okama01@HUGI0255S.adm.huddinge.se>
2026-04-24 09:50:25 +0200

    24/4

---
+++ src/chatprogram/ChatProgram.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package chatprogram;
+import java.net.*;
+import java.io.*;
+
+
+/**
+ *
+ * @author okama01
+ */
+public class ChatProgram {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        
+    }
+    
+}
